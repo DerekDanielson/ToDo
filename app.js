69167f824ef8641aac3e0352f8a0af3a1622e8a3
Derek <derekdanielson84@gmail.com>
2023-04-06 08:25:24 -0600

    Added remove task button

--- app.js
+++ app.js
@@ -1,23 +1,60 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const input = document.getElementById('input');
+const submit = document.getElementById('submit');
+const output = document.getElementById('output');
+const collection = document.getElementById('collection');
+
+// Event listener for submit
+submit.addEventListener('click', addTask);
+    // Add tasks to list
+    function addTask(e){
+        if(input.value === ''){
+            alert('Add Task');
+        } else {
+        // Create li
+        const li = document.createElement('li');
+        // Add class
+        li.className = 'task';
+        // Create text node and append to li
+        li.appendChild(document.createTextNode(input.value));
+        // Append li to collection field
+        collection.appendChild(li);
+        // Create new link element
+        const link = document.createElement('a');
+        // Add class
+        link.className = 'delete-item secondary-content';
+        // Add icon html
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        // Append the link to li
+        collection.appendChild(link);
+        // Assign input value to new li
+        li.innerHTML = input.value;
+        //Clear input
+        input.value = '';
+        }
 
 
 
+        e.preventDefault();
+    };
+
+// Remove task
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Are you sure?')){
+        e.target.parentElement.parentElement.remove();
+        }
+    }
+}
 
 
 
+/*let tasks;
 
-
-
-
-
-const companies = [
-    { name: 'Company One', category: 'Finance', start: 1981, end: 2004 },
-    { name: 'Company Two', category: 'Retail', start: 1992, end: 2008 },
-    { name: 'Company Three', category: 'Auto', start: 1999, end: 2007 },
-    { name: 'Company Four', category: 'Retail', start: 1989, end: 2010 },
-    { name: 'Company Five', category: 'Tech', start: 2009, end: 2014 },
-    { name: 'Company Six', category: 'Finance', start: 1987, end: 2010 },
-    { name: 'Company Seven', category: 'Auto', start: 1986, end: 1996 },
-    { name: 'Company Eight', category: 'Tech', start: 2011, end: 2016 },
-    { name: 'Company Nine', category: 'Retail', start: 1981, end: 1989 }
-];
+tasks.forEach(function(task){
+    //Create li element
+    const li = document.createElement('li');
+    //Add class
+    li.className = 'item';
+    //Append text node to li
+    li.appendChild(document.createTextNode(task));
+});*/
